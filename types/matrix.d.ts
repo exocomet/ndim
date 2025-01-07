aa1eeb4770f92c67706bc2734227384f4030d7cb
Alexander Kutterer <alexander@kutterer.at>
2025-01-07 22:31:22 +0100

    types

---
+++ types/matrix.d.ts
@@ -0,0 +1,22 @@
+import { Vector } from "./vector";
+export declare class Matrix {
+    private arr;
+    constructor(m: Matrix | number[][] | number, n?: number);
+    dim(): number[];
+    copy(): Matrix;
+    isSquare(): boolean;
+    apply(func: any): this;
+    fill(obj: any): this;
+    transpose(): this;
+    get(i: number, j: number): number;
+    set(i: number, j: number, v: number | any): void;
+    getRow(i: number): Vector;
+    getColumn(j: number): Vector;
+    multiply(b: Matrix): this;
+    add(b: Matrix): this;
+    hadamard(b: Matrix): this;
+    scalar(s: number): this;
+    mirrorColumns(): this;
+    mirrorRows(): this;
+    assureArray(a: any): any;
+}
---
+++ types/vector.d.ts
@@ -0,0 +1,17 @@
+export declare class Vector {
+    private arr;
+    constructor(n: number | number[], fill?: number);
+    copy(): Vector;
+    dim(): number;
+    _assertDimensions(v: Vector): void;
+    get(i: number): number;
+    set(i: number, val: any): void;
+    add(v: Vector): this;
+    subtract(v: Vector): this;
+    hadamard(v: Vector): this;
+    dot(v: Vector): number;
+    cross(v: Vector): Vector;
+    multiply(s: number): this;
+    norm(): number;
+    unit(): this;
+}
